61e85a28e333c464d66b765f84fdcaaeee379f3f
HJ Kang <hkang07@gmail.com>
2023-03-31 21:58:43 +0100

    initial commit with class and DOM

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+import { Invoice } from './classes/Invoce.js';
+
+const invOne = new Invoice('mario', 'work on the website', 250);
+const invTwo = new Invoice('luigi', 'work on the website', 300);
+
+
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+
+invoices.forEach(inv => {
+  console.log(inv.client, /* inv.details,*/ inv.amount, inv.format());
+})
+
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+
+const type = document.querySelector('#type') as HTMLSelectElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
+
+  console.log(
+    type.value,
+    tofrom.value,
+    details.value,
+    amount.valueAsNumber
+  );
+})
